694e6018754c7a2a889cd306db0933ad638ae65d
Gulcan Damdelen <61693319+Gulcan00@users.noreply.github.com>
2024-01-10 20:20:54 +0200

    Complete tests for caesarCipher

--- scripts/caesarCipher.js
+++ scripts/caesarCipher.js
@@ -1,4 +1,38 @@
-const plainTextAlphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-export default function caesarCipher(str, shift) {
+const plainTextAlphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+let cipherTextAlphabet = "abcdefghijklmnopqrstuvwxyz".split("");
 
+function shiftArray(shift) {
+  if (shift > 0) {
+    for (let i = 0; i < shift; i++) {
+      const first = cipherTextAlphabet.shift();
+      cipherTextAlphabet.push(first);
+    }
+  } else {
+    for (let i = 0; i < -1 * shift; i++) {
+      const last = cipherTextAlphabet.pop();
+      cipherTextAlphabet.unshift(last);
+    }
+  }
+}
+
+function getIndexes(str) {
+  const strArr = str.split("");
+  return strArr.map((char) => plainTextAlphabet.indexOf(char.toLowerCase()));
+}
+
+export default function caesarCipher(str, shift) {
+  cipherTextAlphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+  shiftArray(shift);
+  const indexes = getIndexes(str);
+  let cipher = "";
+  for (let i = 0; i < str.length; i++) {
+    if (indexes[i] !== -1) {
+      cipher += str[i].match(/[A-Z]/)
+        ? cipherTextAlphabet[indexes[i]].toUpperCase()
+        : cipherTextAlphabet[indexes[i]];
+    } else {
+      cipher += str[i];
+    }
+  }
+  return cipher;
 }
--- tests/caesarCipher.test.js
+++ tests/caesarCipher.test.js
@@ -1,5 +1,13 @@
-import caesarCipher from '../scripts/caesarCipher.js'
+import caesarCipher from "../scripts/caesarCipher.js";
 
-test('shifts a lowercase word', () => 
-    expect(caesarCipher('abc', 3)).toBe('def')
-);
+test("wraps from z to a with lowercase", () =>
+  expect(caesarCipher("xyz", 3)).toBe("abc"));
+
+test("shifts a lowercase word", () =>
+  expect(caesarCipher("abc", 3)).toBe("def"));
+
+test("preserves punctuation and spaces", () =>
+  expect(caesarCipher("Hello, world!", 5)).toBe("Mjqqt, btwqi!"));
+
+test("shifts with negative shift factor", () =>
+  expect(caesarCipher("def", -3)).toBe("abc"));
